e2817998982c75ce1d41fec471dc7f58ac90e007
atooker <atooker93@gmail.com>
2016-06-09 17:20:55 -0700

    Google doodle final

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
 //animate star
 
-var $tri = $('#tri');
+//var $tri = $('#tri');
 
 //DOM element, duration, CSS Properties(all the properties you want to change)
-TweenMax.from($tri, 1, {
-     
-     opacity: 0,
-     x:180
-     
-});
+//TweenMax.from($tri, 1, {
+//     
+//     opacity: 0,
+//     x:180
+//     
+//});
